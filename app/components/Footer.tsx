a0fa03876ce83b7419903fd104eded814a2b2f85
Obayed12 <ullahtarik300@gmail.com>
2025-02-18 18:06:28 +0600

    project udpated

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,33 +1,92 @@
 import Image from "next/image";
-import Logo from "../../public/assets/Logo.svg";
-import Facebook from "../../public/assets/Facebook.svg";
-import Twitter from "../../public/assets/X.svg";
-import Feed from "../../public/assets/Feed.svg";
+import Logo from "../../public/assets/Logo.svg"; 
 
 export function Footer(){
     return(
-        <div className="pt-[80px] pb-[40px]">
-      <div className="flex items-center justify-center gap-x-[12px]">
-        <Image src={Logo} alt="Logo" />
-        <p className="font-bold text-[#36485C] text-[17px]">Obayedullah Khan</p>
+      <footer className="bg-gradient-to-br from-pink-500 via-red-500 to-yellow-500 p-5 rounded-t pb-6 px-10 font-sans tracking-wide relative">
+      <div className="max-w-screen-xl mx-auto">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+          <div>
+            <h2 className="text-white text-sm uppercase font-semibold mb-4">Quick Links</h2>
+            <ul className="space-y-3">
+              <li>
+                <a href="javascript:void(0)" className="text-white hover:text-white text-sm transition-all">Newsroom</a>
+              </li>
+              <li>
+                <a href="javascript:void(0)" className="text-white hover:text-white text-sm transition-all">Tailwind CSS</a>
+              </li>
+              <li>
+                <a href="javascript:void(0)" className="text-white hover:text-white text-sm transition-all">Careers</a>
+              </li>
+            </ul>
+          </div>
+
+          <div>
+            <h2 className="text-white text-sm uppercase font-semibold mb-4">Follow Us</h2>
+            <ul className="space-y-3">
+              <li>
+                <a href="javascript:void(0)" className="text-white hover:text-white text-sm transition-all">Github</a>
+              </li>
+              <li>
+                <a href="javascript:void(0)" className="text-white hover:text-white text-sm transition-all">linkedin</a>
+              </li>
+              <li>
+                <a href="javascript:void(0)" className="text-white hover:text-white text-sm transition-all">Twitter</a>
+              </li>
+            </ul>
+          </div>
+
+          <div>
+            <h2 className="text-white text-sm uppercase font-semibold mb-4">Company</h2>
+            <ul className="space-y-3">
+              <li>
+                <a href="javascript:void(0)" className="text-white hover:text-white text-sm transition-all">About</a>
+              </li>
+              <li>
+                <a href="javascript:void(0)" className="text-white hover:text-white text-sm transition-all">Privacy Policy</a>
+              </li>
+              <li>
+                <a href="javascript:void(0)" className="text-white hover:text-white text-sm transition-all">Terms &amp; Conditions</a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="flex items-center lg:justify-center max-sm:-order-1 mt-10">
+            <a href='javascript:void(0)'><Image height={50} width={50} src={Logo} alt="logo" className='sm:w-16 w-10' /></a>
+          </div>
+        </div>
+
+        <hr className="mt-12 mb-6 border-white" />
+
+        <div className="flex sm:justify-between flex-wrap gap-6">
+          <div className="flex space-x-5">
+            <a href="javascript:void(0)" className="text-white hover:text-white text-sm transition-all">
+              <svg className="w-5 h-5 fill-white hover:fill-white" viewBox="0 0 24 24" aria-hidden="true">
+                <path fill-rule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63
+                  .772-1.63 1.558V12h2.77l-.443 2.89h-2.327V22C18.343 21.128 22 16.991 22 12"></path>
+              </svg>
+            </a>
+            <a href="javascript:void(0)" className="text-white hover:text-white text-sm transition-all">
+              <svg className="w-5 h-5 fill-white hover:fill-white" viewBox="0 0 24 24" aria-hidden="true">
+                <path d="M12 2C6.486 2 2 6.486 2 12c0 5.513 4.486 10 10 10s10-4.487 10-10c0-5.514-4.486-10-10-10zm0 1.542c4.951 0 8.458 3.392 8.458 8.458 0 4.949-3.391 8.458-8.458 8.458-4.948 0-8.458-3.391-8.458-8.458
+                      0-4.949 3.392-8.458 8.458-8.458zM9.743 16.747V7.128l6.027 4.31-6.027 4.309z">
+                </path>
+              </svg>
+            </a>
+            <a href="javascript:void(0)" className="text-white hover:text-white text-sm transition-all">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" className="fill-white hover:fill-white w-5 h-5"
+                viewBox="0 0 24 24">
+                <path fill-rule="evenodd"
+                  d="M21 5a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V5zm-2.5 8.2v5.3h-2.79v-4.93a1.4 1.4 0 0 0-1.4-1.4c-.77 0-1.39.63-1.39 1.4v4.93h-2.79v-8.37h2.79v1.11c.48-.78 1.47-1.3 2.32-1.3 1.8 0 3.26 1.46 3.26 3.26zM6.88 8.56a1.686 1.686 0 0 0 0-3.37 1.69 1.69 0 0 0-1.69 1.69c0 .93.76 1.68 1.69 1.68zm1.39 1.57v8.37H5.5v-8.37h2.77z"
+                  clip-rule="evenodd"></path>
+              </svg>
+            </a>
+          </div>
+
+          <p className='text-white text-sm'>© ReadymadeUI. All rights reserved.
+          </p>
+        </div>
       </div>
-
-      <ul className="flex flex-col items-center gap-y-[32px] pt-[56px] text-[#36485C] sm:flex-row sm:justify-center sm:gap-x-5 sm:pt-5">
-        <li>Features</li>
-        <li>Pricing</li>
-        <li>Enterprise</li>
-        <li>Careers</li>
-      </ul>
-
-      <p className="pt-[56px] text-center text-[14px] font-medium text-[#5F7896] sm:pt-5">
-        © Copyright 2024. Your Site. All rights reserved.
-      </p>
-
-      <div className="flex items-center justify-center gap-x-[56px] pt-[40px]">
-        <Image src={Facebook} alt="Facebook" />
-        <Image src={Feed} alt="Feed" />
-        <Image src={Twitter} alt="Twitter" />
-      </div>
-    </div>
+    </footer>
     )
 }
